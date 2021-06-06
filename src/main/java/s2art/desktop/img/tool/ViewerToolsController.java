0879f9947327ee87e6dab61e31e95b67fd68b339
vvbv <jeison.cardona@correounivalle.edu.co>
2021-06-06 03:36:30 -0500

    U-TGH

---
+++ src/main/java/s2art/desktop/img/tool/ViewerToolsController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s2art.desktop.img.tool;
+import java.lang.String;
+
+/**
+ *
+ * @author vvbv
+ */
+public class ViewerToolsController {
+    
+    public void getImages(){
+    
+    }
+    
+}
